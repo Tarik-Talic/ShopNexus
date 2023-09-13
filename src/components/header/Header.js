075fbad6b125aeb475ef93e28b5a1c8ae85a406f
Tarik-Talic <talic.tarik@gmail.com>
2023-09-13 14:38:10 +0200

    mobile-responsive

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link, NavLink } from "react-router-dom";
 import "../header/Header.css";
 import Logo from "../../assets/Logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 function Header() {
   const linkStyle = {
     textDecoration: "none",
     color: "white",
   };
+  const navRef = useRef();
+  const showNavBar = () => {
+    navRef.current.classList.toggle("responsive-nav");
+  };
   return (
     <>
       <nav>
-        <ul>
+        <ul ref={navRef}>
           <img src={Logo} alt="LogoImage" className="mainLogo" />
 
           <NavLink to="/" style={linkStyle}>
@@ -28,12 +33,18 @@ function Header() {
               <FontAwesomeIcon icon={faCartShopping} />
             </li>
           </NavLink>
+          <NavLink to="/login" style={linkStyle} className="resposive_login">
+            <li> Login </li>
+          </NavLink>
 
           <span className="nav__btns">
             <Link to="/login">
               <button className="nav__btns-login">Login</button>
             </Link>
           </span>
+          <button className="hamBars" onClick={showNavBar}>
+            <FontAwesomeIcon icon={faBars} />
+          </button>
         </ul>
       </nav>
     </>
